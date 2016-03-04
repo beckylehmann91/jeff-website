882c1e2695bf94bfb27204c707425f5f59c19bd7
beckylehmann91 <beckylehmann91@gmail.com>
2016-03-04 12:36:14 -0600

    Add links to forms, back pages

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  include PostHelper
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+end
