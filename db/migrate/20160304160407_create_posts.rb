3c241f390811e3b9d4e13c87b6a2cb546d28a9fc
beckylehmann91 <beckylehmann91@gmail.com>
2016-03-07 10:05:23 -0600

    Edit basic routes (still need to fix 'about' route for 'admin_users#show') and create user and post for seeds

--- db/migrate/20160304160407_create_posts.rb
+++ db/migrate/20160304160407_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :subtitle
       t.text :content
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160304162401) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.string   "subtitle"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
